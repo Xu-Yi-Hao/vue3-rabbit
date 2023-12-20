2e720ed2ecead87070db96672f09dad3a34eea30
徐艺浩 <1182749276@qq.com>
2023-12-20 15:03:02 +0800

    详情页及注册全局组件

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+// 通过插件方式进行通用组件全局注册
+
+import ImageView from './ImageView/index.vue'
+import Sku from './XtxSku/index.vue'
+
+export const componentPlugin = {
+    install(app) {
+        // app.component('组件名字',组件配置对象)
+        app.component('XtxImageView', ImageView)
+        app.component('XtxSku', Sku)
+    }
+}
